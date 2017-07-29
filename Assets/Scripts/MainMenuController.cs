bb42d174f4c96c92f7e4ce6e6bb74ff2c0aa0f41
melc <m.moroz@mailplus.pl>
2017-07-29 21:22:32 +0200

    input manager - added gui input, updated scenes list [GameScene.unity]

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -9,26 +9,12 @@ public class MainMenuController : MonoBehaviour
     
     public void ButtonStart()
     {
-   
-        // audioComponent.button_press();
-        // menuButton.GetComponent<Image>().sprite = menuButton.spriteState.pressedSprite;
-
-        //SceneManager.LoadScene("Gameplay", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
-
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
     }
 
     public void ButtonQuit()
     {
-
-        //		Debug.Log ("LoadMainMenu");
-        //		Application.LoadLevel("menu");
-        //audioComponent.button_press();
-        //		menuButton.spriteState = menuButton.spriteState.pressedSprite;
-        //menuButton.GetComponent<Image>().sprite = menuButton.spriteState.pressedSprite;
-
         Application.Quit();
-
     }
     
 }
