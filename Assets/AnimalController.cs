cb3b9b03e7e4bd0d66e2b9ac746358e3ef4d0c0d
rurush47 <olekrurarz@gmail.com>
2017-07-29 13:50:05 +0200

    Update animal controller to rotate character when moving.

--- Assets/AnimalController.cs
+++ Assets/AnimalController.cs
@@ -1,28 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AnimalController : MonoBehaviour {
 	public float Speed = 6.0F;
-        public float Gravity = 20.0F;
-        private float rot = 0.0f;
-        private Vector3 _moveDirection = Vector3.zero;
-    
-        void Update() {
-            CharacterController controller = GetComponent<CharacterController>();
-            if (controller.isGrounded) {
-                Vector3 forward = Camera.main.transform.TransformDirection(Vector3.forward);
-                forward.y = 0;
-                forward = forward.normalized;
-                Vector3 right  = new Vector3(forward.z, 0, -forward.x);
-                float h = Input.GetAxis("Horizontal");
-                float v =Input.GetAxis("Vertical");
-             
-                _moveDirection  = (h * right  + v * forward);
-                _moveDirection *= Speed;
-            }
- 
-            _moveDirection.y -= Gravity * Time.deltaTime;
-            controller.Move (_moveDirection * Time.deltaTime);
+    public float Gravity = 20.0F;
+    public Vector3 StopRotatingVector;
+    private Vector3 _moveDirection = Vector3.zero;
+
+    void Update() {
+        CharacterController controller = GetComponent<CharacterController>();
+        
+        if (controller.isGrounded) {
+            Vector3 forward = Camera.main.transform.TransformDirection(Vector3.forward);
+            forward.y = 0;
+            forward = forward.normalized;
+            Vector3 right  = new Vector3(forward.z, 0, -forward.x);
+            float h = Input.GetAxis("Horizontal");
+            float v =Input.GetAxis("Vertical");
+         
+            _moveDirection  = (h * right  + v * forward);
+            _moveDirection *= Speed;
         }
+
+        _moveDirection.y -= Time.deltaTime;
+        controller.Move (_moveDirection * Time.deltaTime);
+
+        Vector3 facingrotation = Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"),
+                                                               0f,
+                                                               Input.GetAxis("Vertical")));
+        
+        if (facingrotation != StopRotatingVector)
+        {
+            transform.rotation = Quaternion.LookRotation(-_moveDirection);            
+        }
+    }
 }
