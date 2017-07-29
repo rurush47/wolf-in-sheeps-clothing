944d075801518a19ece372a92ab9e5049dafa2a7
Jan Ambroziak <mordimer7@gmail.com>
2017-07-29 14:12:58 +0200

    Started Sheep manager

---
+++ Assets/SheepBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepBehaviour : MonoBehaviour
+{
+
+	private Vector3 sheepDirection;
+	private Rigidbody sheepRigidBody;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		sheepRigidBody = GetComponent<Rigidbody>();
+		sheepDirection = Quaternion.Euler(0,Random.Range(0,360),0) * Vector3.forward;
+		sheepDirection = sheepDirection.normalized;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		sheepRigidBody.MovePosition(transform.position + sheepDirection*Time.fixedDeltaTime);	
+		
+	}
+}
---
+++ Assets/SheepManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepManager : MonoBehaviour
+{
+	[Range(1, 200)] 
+	public int sheepCount = 20;
+	public GameObject sheepPrefab;
+	public GameObject plane;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		for (int i = 0; i < sheepCount; i++)
+		{
+			Instantiate(sheepPrefab,
+				new Vector3(Random.Range(-50.0f, 50.0f), plane.transform.position.y, Random.Range(-50.0f, 50.0f)),
+				Quaternion.Euler(-90.0f, Random.Range(0, 360), 83.0f));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
