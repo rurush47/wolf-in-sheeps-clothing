fe4ddb8e124c1616771763972722a9aad4a57785
rurush47 <olekrurarz@gmail.com>
2017-07-29 16:40:15 +0200

    Add attack mechanics

---
+++ Assets/Scripts/AttackCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCollider : MonoBehaviour
+{
+
+	private BoxCollider _collider;
+	// Use this for initialization
+	void Start ()
+	{
+		_collider = GetComponent<BoxCollider>();
+	}
+
+	void Update () {
+		
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		Debug.Log("Collides");
+
+		if (other.gameObject.CompareTag("Owca") && Input.GetButton("Jump"))
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Wolf.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Wolf : AnimalController
+{
+    private BoxCollider _attackCollider;
+
+    void Start()
+    {
+        _attackCollider = GetComponent<BoxCollider>();
+    }
+}
